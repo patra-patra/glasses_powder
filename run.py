fd4d0ab8ea9791f664e9b8a3e1ac3fbb7592afa5
bmu11 <shigareva.yana27@gmail.com>
2025-06-09 21:28:51 +1000

    изменение структуры для render. попытка 5

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask, render_template
 from backend.app.models.utils import get_all_products
 
-app = Flask(__name__,
-            static_folder="backend/app/static",
-            template_folder="backend/app/templates")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # папка glasses_powder/
+
+app = Flask(
+    __name__,
+    static_folder=os.path.join(BASE_DIR, "backend", "app", "static"),
+    template_folder=os.path.join(BASE_DIR, "backend", "app", "templates")
+)
 
 @app.route("/")
 def home():
